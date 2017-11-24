0d29feb0162ad0beb1d53ae01afa20958b33b3da
Steels_Frag <danila.yakimov00@gmail.com>
2017-11-24 20:57:55 +1000

    Added Line, Rectangle, Ellips

--- utools.pas
+++ utools.pas
@@ -5,36 +5,104 @@ unit UTools;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
-  ExtCtrls, LCLIntf, LCLType, Buttons, TypInfo, ActnList, UFigures;
+  Classes, SysUtils, Graphics;
 
 type
-  TTools = class
+  TTools = class        //создание массива свойств?
     procedure CreateObject(x,y: integer); Virtual; Abstract;
-    //constructor Create;
-  //rocedure CreateObj;
+    constructor Create;
   end;
 
-  TLineTool = class(TTools)
-   // procedure CreateObject(x,y: integer); override;
-   // constructor CreateObj;
+  TWidth = class(TTools)
+    procedure CreateObject(x,y: integer); override;
+    constructor Create;
+  end;
+
+  TPenColor = class(TTools)
+    procedure CreateObject(x,y: integer); override;
+    constructor Create;
+  end;
+
+  TBrushColor = class(TTools)
+    procedure CreateObject(x,y: integer); override;
+    constructor Create;
+  end;
+
+  TRoundCoef = class(TTools)
+    procedure CreateObject(x,y: integer); override;
+    constructor Create;
   end;
 
 var
   ToolNum: Integer;
-  ToolList: array of TTools;
+  ToolList: array of TTools;   // нужно ли?
 
 implementation
-uses UMain;
-///////////////////////////////////////////////////////////////////
-{procedure TLineTool.CreateObject(x,y: integer);
-begin
-  SetLength(CanvasFigures,Length(CanvasFigures)+1);
-  CanvasFigures[Length(CanvasFigures)]:= TLine.Create;
-  CanvasFigures[Length(CanvasFigures)].Points[0]:= Point(x,y);
-  CanvasFigures[Length(CanvasFigures)].Points[1]:= Point(x,y);
-end;  }
 
+constructor TTools.Create;
+begin
+  case ToolNum of
+    1,2:
+       begin
+         TPenColor.Create;
+         TWidth.Create;
+       end;
+    3:
+      begin
+        TPenColor.Create;
+        TWidth.Create;
+       // TBrushColor;
+      //  TRoundCoef; //Коэфицент скругления
+      end;
+    4,5:
+       begin
+         TPenColor.Create;
+         TWidth.Create;
+      //8   TBrushColor;
+       end;
+
+   end;
+  end;
+
+constructor TPenColor.Create;
+begin
+
+end;
+
+constructor TWidth.Create;
+begin
+
+end;
+
+constructor TBrushColor.Create;
+begin
+
+end;
+
+constructor TRoundCoef.Create;
+begin
+
+end;
+
+procedure TPenColor.CreateObject(x,y: integer);
+begin
+
+end;
+
+procedure TWidth.CreateObject(x,y: integer);
+begin
+
+end;
+
+procedure TBrushColor.CreateObject(x,y: integer);
+begin
+
+end;
+
+procedure TRoundCoef.CreateObject(x,y: integer);
+begin
+
+end;
 
 end.
 
